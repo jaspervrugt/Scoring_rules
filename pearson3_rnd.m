c02af3e151771b828088d9111e75190138ffc363
Jasper A. Vrugt <106767445+jaspervrugt@users.noreply.github.com>
2024-06-26 04:27:47 -0700

    Add files via upload

--- pearson3_rnd.m
+++ pearson3_rnd.m
@@ -1,55 +1,50 @@
+function X = pearson3_rnd(alpha,beta,xi,rho,m,n)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compute a random sample of m x n values from the Pearson 3 distribution. 
+% The Hosking and Wallis (1997) version of the distribution is chosen.  
 %
-% Compute a random sample of (m * n) values, using the the Pearson 3
-% distribution. The Hosking and Wallis (1997) version of the distribution
-% is chosen.  
+% Given the location (mu), scale (sigma) and shape (rho) parameters, the 
+% three other parameters used in this version are
+%   alpha = 4 / sigma^2
+%   beta = 0.5 * sigma * abs(rho)
+%   xi = mu - 2*sigma/rho
 %
-% Given the location (mu), scale (sigma) and shape (Gamma) parameters, we can
-% estimate the three other parameters used in this version :
-%   -alpha : 4 / sigma^2
-%   -beta :0.5 * sigma * abs(Gamma)
-%   -xi : mu - 2*sigma/Gamma
+% If rho > 0, the range of x is : xi <= x < Inf 
+% If rho = 0, the range of x is : -Inf < x < Inf 
+% If rho < 0, the range of x is : -Inf < x <= xi 
 %
-% If Gamma  > 0, the range of x is : xi <= x < Inf 
-% If Gamma  = 0, the range of x is : -Inf < x < Inf 
-% If Gamma  < 0, the range of x is : -Inf < x <= xi 
+% Input arguments
+%    alpha, beta, xi        parameters of the distribution
+%    rho                    shape parameter. Depending on its value, the 
+%                           skewness is positive (rho > 0) or negative 
+%                           (rho < 0). If rho = 0, the distribution is 
+%                           normal where the mean is alpha and the standard
+%                           deviation is beta
+%    m, n                   number of rows and columns of matrix X 
+% Output argument(s)
+%    X                      m x n matrix of random samples from PIII(xi,a,b)
 %
-% Input :
-%    -alpha, beta, xi : parameters of the distribution
-%    -Gamma : shape parameter. Depending on its value, the skewness is
-%       positive (Gamma > 0) or negative (Gamma < 0). If Gamma = 0, the
-%       distribution is normal where the mean is alpha and the standard
-%       deviation is beta
-%    -m, n : number of rows (m) and colums (n) of the output matrix
-%
-% Output
-%   -x : matrix of the random sample
-%
-% Source : Hosking, J., & Wallis, J. (1997). Regional Frequency Analysis:
-% An Approach Based on L-Moments. Cambridge: Cambridge University Press. 
-% doi:10.1017/CBO9780511529443
+% Reference: 
+%   Hosking, J., & Wallis, J. (1997). Regional Frequency Analysis:
+%       An Approach Based on L-Moments. Cambridge: Cambridge University Press. 
+%       doi:10.1017/CBO9780511529443
 %
 % Guillaume Talbot, INRS-ETE 2021
+% modified by Jasper A. Vrugt, July 2021
+% UCI
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function [x]=pearson3_rnd(alpha,beta,xi,Gamma,m,n)
+% Generate a m x n matrix with values between 0 and 1
+p = rand(m,n);
 
-%% Generate a random matrix (m * n) with values between 0 and 1
-p=rand(m,n);
-
-%% If Gamma = 0, the Normal distribution is used
-if Gamma==0 
-    norm_inv=@(P,mu,sigma) sigma.*(-sqrt(2).*erfcinv(2.*P))+mu;
-    x=norm_inv(p,alpha,beta);    
-    return
-end
-
-%% Compute the inverse cdf of the matrix p to generate the random sample
-if Gamma>0 %Case of positive skewness    
-    X=gammaincinv(p,alpha.*ones(size(p)));
-    x=(X.*beta)+xi;  
-else %Case of negative skewness    
-    X=gammaincinv(1-p,alpha.*ones(size(p)));    
-    x=(-X.*beta)+xi;
+% Draw a matrix X of size m x n with samples from PIII(xi,a,b)
+if rho == 0 % No skewness, draw from normal distribution
+    X = norminv(p,alpha,beta);    
+elseif rho > 0 % Positive skewness    
+    X = gammaincinv(p,alpha.*ones(size(p)));
+    X = X.*beta + xi;  
+else % Negative skewness    
+    X = gammaincinv(1-p,alpha.*ones(size(p)));    
+    X = - X.*beta + xi;
 end
